48497047cca4e8810a18824dfdf42c6b9473a49d
ShivamVerma380 <71377475+ShivamVerma380@users.noreply.github.com>
2022-05-06 12:42:17 +0530

    Added crossorigin

--- src/main/java/com/brewingjava/mahavir/MahavirApplication.java
+++ src/main/java/com/brewingjava/mahavir/MahavirApplication.java
@@ -2,7 +2,10 @@ package com.brewingjava.mahavir;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableMongoRepositories
@@ -13,4 +16,5 @@ public class MahavirApplication {
 		
 	}
 
+	
 }
--- src/main/java/com/brewingjava/mahavir/controllers/EmailVerification.java
+++ src/main/java/com/brewingjava/mahavir/controllers/EmailVerification.java
@@ -5,23 +5,33 @@ import com.brewingjava.mahavir.services.EmailVerificationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 public class EmailVerification {
     
     @Autowired
     public EmailVerificationService emailVerificationService;
 
-    @GetMapping("/verify-email")
-    public ResponseEntity<?> getOTP(@RequestParam("email")String email){
+    @GetMapping("/verify-email/{email}")
+    public ResponseEntity<?> getOTP(@PathVariable("email")String email){
         try {
             return emailVerificationService.getOtp(email); 
         } catch (Exception e) {
             e.printStackTrace();
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
         }
+    }
+
+    
+    @GetMapping("/welcome")
+    public ResponseEntity<?> getString(){
+        return ResponseEntity.ok().body("Hello World");
     }
 }
