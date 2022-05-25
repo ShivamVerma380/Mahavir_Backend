f8d6c09d52870c79378c91e9f8ffbe6e06f48c96
ShivamVerma380 <71377475+ShivamVerma380@users.noreply.github.com>
2022-05-25 15:57:56 +0530

    Update MySecurityConfig.java

--- src/main/java/com/brewingjava/mahavir/config/MySecurityConfig.java
+++ src/main/java/com/brewingjava/mahavir/config/MySecurityConfig.java
@@ -43,7 +43,7 @@ public class MySecurityConfig extends WebSecurityConfigurerAdapter {
 
     http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
     }
-
+ //***************
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception{
