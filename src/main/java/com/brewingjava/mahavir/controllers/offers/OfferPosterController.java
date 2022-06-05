77f2062f4f9c6f39ea0181278bb6f8b40c058240
= <shivamvermasv380@gmail.com>
2022-06-06 02:26:50 +0530

    Fixed get Products by model number

--- src/main/java/com/brewingjava/mahavir/controllers/offers/OfferPosterController.java
+++ src/main/java/com/brewingjava/mahavir/controllers/offers/OfferPosterController.java
@@ -10,6 +10,7 @@ import com.brewingjava.mahavir.services.offers.OfferPosterService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,7 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
-//@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "*")
 public class OfferPosterController {
     
     @Autowired
