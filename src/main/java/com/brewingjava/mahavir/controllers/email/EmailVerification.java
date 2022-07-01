3f80d289cf94db9f4503161fac333e45aff289c3
= <shivamvermasv380@gmail.com>
2022-07-01 18:46:35 +0530

    Added resend otp api

--- src/main/java/com/brewingjava/mahavir/controllers/email/EmailVerification.java
+++ src/main/java/com/brewingjava/mahavir/controllers/email/EmailVerification.java
@@ -61,6 +61,37 @@ public class EmailVerification {
         }
     }
 
+    @GetMapping("/resend-otp/{email}")
+    public ResponseEntity<?> resendOTP(@PathVariable("email") String email){
+        try {
+            Random random = new Random();
+            int number = random.nextInt(999999);
+
+            String otp = String.format("%06d", number);
+            
+            Mail mail = new Mail();
+            mail.setFrom("shivam380.testing@gmail.com");
+            mail.setTo(email);
+            mail.setSubject("Mahavir Electronics-New OTP");
+
+
+            Map model = new HashMap();
+            model.put("name", "Customer,");
+            model.put("location", "Belgium");
+            model.put("signature", "https://memorynotfound.com");
+            model.put("otp", otp);
+            mail.setModel(model);
+
+            emailVerificationService.sendSimpleMessage(mail);
+            otpResponse.setMessage("Email sent successfully");
+            otpResponse.setOtp(otp);
+            return ResponseEntity.status(HttpStatus.OK).body(otpResponse);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+        }
+    }
+
     @GetMapping("/forgotPassword/{email}")
     public ResponseEntity<?> forgotPassword(@PathVariable("email")String email){
         try {
